d64f2a95f65eca08d2ba45a9b07c774eff430af8
lahousethings <50772627+lahousethings@users.noreply.github.com>
2022-07-04 13:48:11 +0300

    The damn note

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,17 +1,9 @@
 from seleniumbase import BaseCase
 
 class HomeTest(BaseCase):
-    def test_home_page(self):
 
-        self.open("https://practice.automationbro.com/")
+    def test_temperature(self):
 
-        self.assert_title("Practice E-Commerce Site – Automation Bro")
-
-        self.click("#get-started")
-        get_started_url = self.get_current_url()
-        self.assert_equal(get_started_url,"https://practice.automationbro.com/#get-started")
-
-        self.assert_text("Think different. Make different." , "h1")
-
-        self.scroll_to_bottom()
-        self.assert_text("Copyright © 2020 Automation Bro",".tg-site-footer-section-1")
+        self.open("https://www.worldweatheronline.com/geri-weather/nicosia/cy.aspx")
+        self.get_title()
+        print(self.get_text("div.weather-widget-temperature"))
