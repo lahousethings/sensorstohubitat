4c5af420d300b70f31942eb545bafe01104c6b04
lahousethings <50772627+lahousethings@users.noreply.github.com>
2022-07-07 10:17:11 +0300

    The damn note

--- tests/test_home.py
+++ tests/test_home.py
@@ -13,12 +13,24 @@ class HomeTest(BaseCase,Keys):
     def test_pool_temperature(self):
 
         self.open("https://oxymaticapp.hydrover.eu/")
-        self.send_keys("user","Lahome")
-        self.send_keys("password","8mIHfx4A")
+        self.send_keys("name=user","Lahome")
+        self.send_keys("name=password","8mIHfx4A")
 
-        self.click("send")
+        self.click(".send")
+        self.click("//*[@main-id=2052]")
+        self.wait(15)
+        pool_numbers = self.find_elements("//*[starts-with(@class,'big-number')]")
+        for index, pool_number in enumerate(pool_numbers,1):
+            if index == 1:
+                pool_number_1 = pool_number.text.replace(",",".")
+                print(pool_number_1)
+                self.open_url("http://192.168.90.254/apps/api/1241/devices/7041/setTemperature/"+pool_number_1+"?access_token=d1303322-7a9c-474a-a385-b37ef32b2103")
 
-    def test_cmi_temperature(self):
+
+
+
+
+def test_cmi_temperature(self):
 
         #Login
         self.open("https://cmi.ta.co.at/")
