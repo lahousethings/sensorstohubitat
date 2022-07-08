a75c57dcc7e2999f99b29167a2582f804c717d8d
lahousethings <50772627+lahousethings@users.noreply.github.com>
2022-07-08 10:08:31 +0300

    Everything working :)

--- tests/test_home.py
+++ tests/test_home.py
@@ -11,26 +11,28 @@ class HomeTest(BaseCase,Keys):
         self.open_url("http://192.168.90.254/apps/api/1241/devices/7257/setTemperature/"+geriweather+"?access_token=d1303322-7a9c-474a-a385-b37ef32b2103")
 
     def test_pool_temperature(self):
-
+        #Login
         self.open("https://oxymaticapp.hydrover.eu/")
         self.send_keys("name=user","Lahome")
         self.send_keys("name=password","8mIHfx4A")
 
+        #Get Pool Numbers
         self.click(".send")
         self.click("//*[@main-id=2052]")
-        self.wait(15)
+        self.wait(10)
+        poollist = ["List"]
         pool_numbers = self.find_elements("//*[starts-with(@class,'big-number')]")
-        for index, pool_number in enumerate(pool_numbers,1):
-            if index == 1:
-                pool_number_1 = pool_number.text.replace(",",".")
-                print(pool_number_1)
-                self.open_url("http://192.168.90.254/apps/api/1241/devices/7041/setTemperature/"+pool_number_1+"?access_token=d1303322-7a9c-474a-a385-b37ef32b2103")
+        for index, pool_number in enumerate(pool_numbers, 1):
+            poollist.append(pool_number.text.replace(",", "."))
 
+        #Send Numbers to Hubitat
+        self.open_url("http://192.168.90.254/apps/api/1241/devices/7041/setTemperature/"+poollist[1]+"?access_token=d1303322-7a9c-474a-a385-b37ef32b2103")
+        self.open_url("http://192.168.90.254/apps/api/1241/devices/7048/setTemperature/"+poollist[2]+"?access_token=d1303322-7a9c-474a-a385-b37ef32b2103")
+        self.open_url("http://192.168.90.254/apps/api/1241/devices/7049/setTemperature/"+poollist[3]+"?access_token=d1303322-7a9c-474a-a385-b37ef32b2103")
+        self.open_url("http://192.168.90.254/apps/api/1241/devices/7046/setTemperature/"+poollist[6]+"?access_token=d1303322-7a9c-474a-a385-b37ef32b2103")
+        self.open_url("http://192.168.90.254/apps/api/1241/devices/7047/setTemperature/"+poollist[7]+"?access_token=d1303322-7a9c-474a-a385-b37ef32b2103")
 
-
-
-
-def test_cmi_temperature(self):
+    def test_cmi_temperature(self):
 
         #Login
         self.open("https://cmi.ta.co.at/")
